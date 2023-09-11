7105c739a58cb966be27ff4cfd2eafd0880c0036
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-09-11 12:26:31 +0200

    Add project files.

---
+++ H1_Superheroes/Model/Ability.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Superheroes
+{
+    /// <summary>
+    /// Class <see cref="Ability"/> represents a ability or abilities which a superhuman would be able to have
+    /// </summary>
+    internal class Ability
+    {
+        #region Fields
+        private string _name;
+        private string _description;
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ability"/> class, using multiple arguments
+        /// </summary>
+        /// <param name="name">name of ability</param>
+        /// <param name="description">description of ability</param>
+        public Ability(string name, string description)
+        { 
+            _name = name;
+            _description = description;
+        }
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        #endregion
+    }
+}
---
+++ H1_Superheroes/Model/Superhuman.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Superheroes
+{
+    /// <summary>
+    /// Class <see cref="Superhuman"/> represents a superhuman and contains attributes that define a superhuman
+    /// </summary>
+    internal class Superhuman
+    {
+        private string _identity;
+        private Ability[] _abilities;
+        private string[] _equipment;
+        private string[] _suit;
+        private string[] _league;
+        private string _moralCode;
+        private Superhuman _archenemy;
+        private string _backgroundStory;
+
+        /// <summary>
+        /// Name of the superhuman
+        /// </summary>
+        public string Identity
+        {
+            get { return _identity; }
+            set { _identity = value; }
+        }
+        public Ability[] Abilities
+        {
+            get { return _abilities; }
+            set { _abilities = value; }
+        }
+        public string[] Equipment
+        {
+            get { return _equipment; }
+            set { _equipment = value; }
+        }
+        public string[] Suit
+        {
+            get { return _suit; }
+            set { _suit = value; }
+        }
+        public string[] League
+        {
+            get { return _league; }
+            set { _league = value; }
+        }
+        public string MoralCode
+        {
+            get { return _moralCode; }
+            set { _moralCode = value; }
+        }
+        public Superhuman Archenemy
+        {
+            get { return _archenemy; }
+            set { _archenemy = value; }
+        }
+
+        /// <summary>
+        /// Story of the superhuman, and the daily problems they have.
+        /// </summary>
+        public string BackgroundStory
+        {
+            get { return _backgroundStory; }
+            set { _backgroundStory = value; }
+        }
+    }
+}
